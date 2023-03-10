405432d88d70cabc0c4bfc99acff9a5615fa6451
Edouard Godineau <95038026+edouardgodineau@users.noreply.github.com>
2023-03-10 18:56:13 +0100

    designed pages formatting function to deal with equic and values concatenate function ready to build the recommendation

--- pages.py
+++ pages.py
@@ -1,79 +1,86 @@
 import pandas as pd
 import numpy as np
+import re
 
+pd.set_option('display.max_columns', None)
 data = pd.read_excel('Recommendations__.xlsx', sheet_name="Amide_coupling_App")
-# print(data.head())
-data['amine_equiv_'] = data.amine_equiv.apply(lambda x: str(x))
-# data['recommendation'] = data.carboxylic_acid + '(1 equiv.), ' +\
-#                          data.amine + '(' + data.amine_equiv_ +' equiv.),'+ data.base + data.Column1 + data.coupling_agent +
-
-for col in data.select_dtypes(np.number):
-col_ = f'{col}_'
-if "equiv" in col:
-    data[col] = data[col].apply(lambda x: '(' + str(x) + ' eq.)')
-else:
-    data[col] = data[col].apply(lambda x: str(x))
-
-# print(data.dtypes)
 
 
-cols_recommended = ['carboxylic_acid',
-                'carboxylic_acid_equiv',
-                'amine',
-                'amine_equiv',
-                'base',
-                'base_equiv',
-                'coupling_agent',
-                'coupling_agent_equiv',
-                'Solvent',
-                'Temperature',
-                'Reference',
-                'ELN',
-                'Comments'
-                ]
+def format_data(df: pd.DataFrame) -> pd.DataFrame:
+    equiv = [col for col in df.columns if "equiv" in col]
+    unit = [col for col in df.columns if "unit" in col]
+    for col in equiv:
+        df[col] = " ("+ df[col].astype(str) + " eq.)" if df[col].notna().all() else ""
+    for col in unit:
+        df[col] = " " + df[col].astype(str) if df[col].notna().all() else ""
+    return df
 
-prefixes = {}
-
-# Loop through each column name
-for col_name in cols_recommended:
-# Extract the prefix of the column name
-prefix = col_name.split('_')[0]
-
-# Add the column name to the prefix dictionary
-if prefix in prefixes:
-    prefixes[prefix].append(col_name)
-else:
-    prefixes[prefix] = [col_name]
-
-# Create a list of pairs from the prefix dictionary
-column_pairs = [v for v in prefixes.values()]
-
-cols = []
-for v in column_pairs:
-data[v[0]] = data[v].apply(lambda x: " ".join(x.astype(str)), axis=1)
-cols.append(v[0])
-
-# View the resulting column pairs
-
-# data['recommendation'] = data[cols].apply(lambda x: ', '.join(x.dropna().astype(str)), axis=1)
-data['recommendation'] = data[cols].apply(lambda x: ', '.join([str(i) for i in x if pd.notna(i) and str(i) != 'nan']), axis=1)
+data = format_data(data)
 
 
-df = data.recommendation
-df.to_json('recommendations4amide.json')
+def build_pairs(df: pd.DataFrame) -> list:
+    """
+    Build pairs of columns from a dataframe
+    :param df: dataframe
+    :return: list of pairs
+    """
+    params = [col.split('_')[0] for col in df.columns if "_equiv" in col or "_value" in col]
+    cols = []
+    for param in params:
+        param_value_unit = [col for col in df.columns if param in col]
+        cols.append(param_value_unit)
+    return cols
 
 
-import json
+def prepare_params(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Prepare parameters for the recommendation
+    :param df: dataframe
+    :return: dictionary of parameters
+    """
+    params = build_pairs(df)
+
+    for param in params:
+        to_drop = [col for col in param if col not in param[0]]
+
+        df[param[0]] = df[param].apply(lambda x: ''.join(x.dropna().astype(str)), axis=1)
+        if "_value" in param[0]:
+            col = re.sub("_value", "", param[0])
+            df[col] = df[param[0]]
+            to_drop.append(param[0])
+
+        df = df.drop(to_drop, axis=1)
+    return df
 
 
-with open('recommendations4amide.json', 'r') as f:
-data = json.loads(f.read())
+data = prepare_params(data)
+print(data)
 
-amide1 = []
-for i in range(3):
-reco = f'Top{i+1} condition: {data[str(i)]}'
-amide1.append(reco)
 
-print(amide1)
 
-def proce
+
+# # View the resulting column pairs
+#
+# # data['recommendation'] = data[cols].apply(lambda x: ', '.join(x.dropna().astype(str)), axis=1)
+# data['recommendation'] = data[cols].apply(lambda x: ', '.join([str(i) for i in x if pd.notna(i) and str(i) != 'nan']), axis=1)
+#
+# print(data['recommendation'].head())
+
+# df = data.recommendation
+# df.to_json('recommendations4amide.json')
+#
+#
+# import json
+#
+#
+# with open('recommendations4amide.json', 'r') as f:
+#     data = json.loads(f.read())
+#
+# amide1 = []
+# for i in range(3):
+#     reco = f'Top{i+1} condition: {data[str(i)]}'
+#     amide1.append(reco)
+#
+# print(amide1)
+
+
